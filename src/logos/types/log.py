9c92c876550712dd99d04469c07e2bda49f32fdb
Harvey Devereux <h.devereux@qmul.ac.uk>
2025-10-29 09:42:26 +0000

    No longer include categories

--- src/logos/types/log.py
+++ src/logos/types/log.py
@@ -7,27 +7,22 @@ from logos.types.entry import Entry
 
 class Log:
     def __init__(self, root: Path):
-        self.categories = self._get_dirs(root)
-        self.entries = dict()
 
-        for category in self.categories:
-            self.entries[category] = []
-            for year in self._get_dirs(category):
-                for month in self._get_dirs(year):
-                    for entry in self._get_files(month):
-                        self.entries[category].append(Entry(entry))
-
+        self._root = root
+        self.entries = []
         self._task_to_entry = dict()
-        for category in self.categories:
-            for entry in self.entries[category]:
-                for task in entry.tasks.values():
-                    self._task_to_entry[task.hash] = entry
-
         hashes = []
-        for category in self.categories:
-            for entry in self.entries[category]:
-                for task in entry.tasks.values():
-                    hashes.append(task.hash)
+        for year in self._get_dirs(root):
+            for month in self._get_dirs(year):
+                for entry in self._get_files(month):
+                    self.entries.append(Entry(entry))
+
+        for entry in self.entries:
+            for task in entry.tasks.values():
+                self._task_to_entry[task.hash] = entry
+                hashes.append(task.hash)
+
+        self._entry_by_date = {entry.date: entry for entry in self.entries}
 
         m = min(len(h) for h in hashes)
         self._prune_length = m
@@ -49,38 +44,35 @@ class Log:
     def show_tasks(self, show_complete: bool):
         today = date.today()
 
-        for category in self.categories:
-            outstanding = 0
-            complete = 0
-            tasks = dict()
-            for entry in self.entries[category]:
-                if len(entry.tasks) > 0:
-                    tasks[entry.date] = []
-                    for _, task in entry.tasks.items():
-                        tasks[entry.date].append(task)
-                        outstanding += not task.complete
-                        complete += task.complete
+        outstanding = 0
+        complete = 0
+        tasks = dict()
+        for entry in self.entries:
+            if len(entry.tasks) > 0:
+                tasks[entry.date] = []
+                for _, task in entry.tasks.items():
+                    tasks[entry.date].append(task)
+                    outstanding += not task.complete
+                    complete += task.complete
 
-            print(f"{category}:\n Outstanding tasks: {outstanding}")
+        print(f"{self._root}:\n Outstanding tasks: {outstanding}")
+        for start_date in tasks:
+            days_past = (today-start_date).days
+            for task in tasks[start_date]:
+                if not task.complete:
+                    print(f"    Age: {days_past} days ({start_date})")
+                    print(f"      [0x{task.hash[:self._prune_length]}] {task.name} ")
+
+        if show_complete:
+            print(f"\n Complete tasks: {complete}")
             for start_date in tasks:
-                days_past = (today-start_date).days
                 for task in tasks[start_date]:
-                    if not task.complete:
-                        print(f"    Age: {days_past} days ({start_date})")
+                    if task.complete:
+                        print(f"    Dated: {start_date}")
                         print(f"      [0x{task.hash[:self._prune_length]}] {task.name} ")
 
-            if show_complete:
-                print(f"\n Complete tasks: {complete}")
-                for start_date in tasks:
-                    for task in tasks[start_date]:
-                        if task.complete:
-                            print(f"    Dated: {start_date}")
-                            print(f"      [0x{task.hash[:self._prune_length]}] {task.name} ")
-
     def lastest(self) -> Entry:
-        for category in self.categories:
-            entry_by_date = {entry.date: entry for entry in self.entries[category]}
-            return entry_by_date[list(reversed(sorted(entry_by_date)))[0]]
+        return self._entry_by_date[list(reversed(sorted(self._entry_by_date)))[0]]
 
     def setComplete(self, hash: str, complete: bool) -> None:
         hash = self._to_full_hash(hash)
